b4616b31c111f2fe5604114936608598d26eb570
TrevonK-coder <trevonkip@gmail.com>
2026-02-25 08:58:11 -0500

    Complete Zoo Assignment 3

--- Zoo_Assignment.sql
+++ Zoo_Assignment.sql
@@ -3,3 +3,15 @@
 */
 
 SELECT * FROM tbl_habitat;
+/* Zoo Database Assignment 2 
+   Task: Retrieve only the habitat_name and habitat_size from the habitat table.
+*/
+SELECT habitat_name, habitat_size FROM tbl_habitat;
+/* Zoo Database Assignment 3 
+   Task: Retrieve only the types from the column nutrition_type of the tbl_nutrition table 
+   that have a nutrition_cost of 600.00 or less.
+*/
+
+SELECT nutrition_type 
+FROM tbl_nutrition 
+WHERE nutrition_cost <= 600.00;
